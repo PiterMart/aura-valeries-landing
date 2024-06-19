bb2e1e00c41c4892e10a552a2ad4f69e8287b4d6
Zyntax <pitermartingaste@gmail.com>
2024-06-19 20:25:45 -0300

    fixing last commit

--- src/app/components/newsletter.js
+++ src/app/components/newsletter.js
@@ -1,6 +1,6 @@
 "use client"
-import { useEffect } from "react";
-import $ from 'jquery';
+// import { useEffect } from "react";
+// import $ from 'jquery';
 import styles from "../page.module.css";
 
 export default function Newsletter() {
