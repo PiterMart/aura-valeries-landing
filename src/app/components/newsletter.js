7dd51e313a60a14c48b35580050d762d82b71aa8
Zyntax <pitermartingaste@gmail.com>
2024-06-19 20:45:38 -0300

    newsletter

--- src/app/components/newsletter.js
+++ src/app/components/newsletter.js
@@ -1,42 +1,42 @@
-"use client"
+// "use client"
 // import { useEffect } from "react";
 // import $ from 'jquery';
-import styles from "../page.module.css";
+// import styles from "../page.module.css";
 
-export default function Newsletter() {
+// export default function Newsletter() {
 
-    return (
-    <div>
-        <div id="mc_embed_shell" className={styles.newsletter}>
-            <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
+//     return (
+//     <div>
+//         <div id="mc_embed_shell" className={styles.newsletter}>
+//             <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
 
-            <div id="mc_embed_signup" style={{ background:'#F73939', clear: 'left', font:'14px Helvetica,Arial,sans-serif', width: '300px'}}>
-                <form action="https://valeriesfactory.us10.list-manage.com/subscribe/post?u=b1625203d7814328871f87895&amp;id=ddc428d16c&amp;f_id=001881e5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-                    <div id="mc_embed_signup_scroll"><h2>Suscribite al newsletter</h2>
-                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                        <div class="mc-field-group"><label for="mce-EMAIL">Dirección de correo electrónico <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""/></div>
-                        <div hidden=""><input type="hidden" name="tags" value="14375455"/></div>
-                        <div id="mce-responses" class="clear foot">
-                            <div class="response" id="mce-error-response" style={{display: 'none'}}></div>
-                            <div class="response" id="mce-success-response" style={{display: 'none'}}></div>
-                        </div>
-                        <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-                            /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
-                            <input type="text" name="b_b1625203d7814328871f87895_ddc428d16c" tabindex="-1" value=""/>
-                        </div>
-                        <div class="optionalParent">
-                            <div class="clear foot">
-                                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"/>
-                                <p style={{margin: '0px auto'}}><a href="http://eepurl.com/iLLjt2" title="Mailchimp: marketing por correo electrónico fácil y divertido"><span style={{display: 'inline-block', backgroundColor: 'black', borderRadius: '4px'}}></span></a></p>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-        </div>
-    </div>
+//             <div id="mc_embed_signup" style={{ background:'#F73939', clear: 'left', font:'14px Helvetica,Arial,sans-serif', width: '300px'}}>
+//                 <form action="https://valeriesfactory.us10.list-manage.com/subscribe/post?u=b1625203d7814328871f87895&amp;id=ddc428d16c&amp;f_id=001881e5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+//                     <div id="mc_embed_signup_scroll"><h2>Suscribite al newsletter</h2>
+//                         <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+//                         <div class="mc-field-group"><label for="mce-EMAIL">Dirección de correo electrónico <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""/></div>
+//                         <div hidden=""><input type="hidden" name="tags" value="14375455"/></div>
+//                         <div id="mce-responses" class="clear foot">
+//                             <div class="response" id="mce-error-response" style={{display: 'none'}}></div>
+//                             <div class="response" id="mce-success-response" style={{display: 'none'}}></div>
+//                         </div>
+//                         <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
+//                             /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+//                             <input type="text" name="b_b1625203d7814328871f87895_ddc428d16c" tabindex="-1" value=""/>
+//                         </div>
+//                         <div class="optionalParent">
+//                             <div class="clear foot">
+//                                 <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"/>
+//                                 <p style={{margin: '0px auto'}}><a href="http://eepurl.com/iLLjt2" title="Mailchimp: marketing por correo electrónico fácil y divertido"><span style={{display: 'inline-block', backgroundColor: 'black', borderRadius: '4px'}}></span></a></p>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </form>
+//             </div>
+//             <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+//         </div>
+//     </div>
 
-    );
-}
+//     );
+// }
 
