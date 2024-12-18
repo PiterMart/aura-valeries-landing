f7615f9ae311f7c014a1341f2769a993d5bbe544
Zyntax <pitermartingaste@gmail.com>
2024-12-18 13:58:26 -0300

    contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -40,11 +40,7 @@ export default function Artists() {
                   </a>
                 </p>
               </div>
-            </div>
-          </div>
-          <div className={styles.contentSection}>
-            <p className={styles.subtitle}>PRODUCCIÓN EJECUTIVA</p>
-            <div className={styles.contactInfo}>
+              <div className={styles.contactInfo}>
               <p className={styles.subtitle}>
                  Sandro Guevara
               </p>
@@ -54,7 +50,11 @@ export default function Artists() {
                 </a>
               </p>
             </div>
+            </div>
           </div>
+          {/* <div className={styles.contentSection}>
+            <p className={styles.subtitle}>PRODUCCIÓN EJECUTIVA</p>
+          </div> */}
           {/* <div className={styles.contentSection}>
 
             <p className={styles.subtitle}>PRODUCCIÓN ARTÍSTICA</p>
